66b5fe950dd1d0d41bac1487373ca73c2d96380d
HaekyungSeong <haekyungs95@gmail.com>
2023-04-21 23:10:14 +0900

    currency 데이터 모듈화

---
+++ 리팩토링버전/currency.js
@@ -0,0 +1,9 @@
+const currency = {
+  thousand: { value: 1000, name: "일천원권" },
+  fiveThousand: { value: 5000, name: "오천원권" },
+  tenThousand: { value: 10000, name: "일만원권" },
+  fiveHundred: { value: 500, name: "오백원" },
+  hundred: { value: 100, name: "일백원" }
+};
+
+export default currency
